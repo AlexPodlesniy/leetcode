68e17f945a3ef45d815b71ff680d9a7129965bbe
Alex <apodlesnyj@yandex.ru>
2025-09-30 21:29:42 +0300

    4. Median of Two Sorted Arrays - binary search

--- algorithms/to100/to10/4_MedianOfTwoSortedArrays.java
+++ algorithms/to100/to10/4_MedianOfTwoSortedArrays.java
@@ -1,6 +1,7 @@
 package to100.to10;
 
-class MedianOfTwoSortedArrays {
+class MergeAndGetMedian {
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] mergedArr = mergeArrs(nums1, nums2);
 
@@ -27,5 +28,54 @@ class MedianOfTwoSortedArrays {
             }
         }
         return arr;
+    }
+}
+
+class BinarySearchMedian {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int half = (n + m) / 2;
+
+        // ищем середину
+        int low = 0;
+        int high = n;
+
+        int mid1 = -1;
+        int mid2 = -1;
+        int l1, l2, r1, r2;
+        double leftMax, rightMin;
+        while (true) {
+            l1 = Integer.MIN_VALUE;
+            l2 = Integer.MIN_VALUE;
+            r1 = Integer.MAX_VALUE;
+            r2 = Integer.MAX_VALUE;
+
+            mid1 = (low + high) / 2;
+            mid2 = half - mid1;
+
+            if (mid1 > 0) l1 = nums1[mid1 - 1];
+            if (mid1 < n) r1 = nums1[mid1];
+            if (mid2 > 0) l2 = nums2[mid2 - 1];
+            if (mid2 < m) r2 = nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 1) {
+                    return Math.min(r1, r2);
+                } else {
+                    double r = Math.min(r1, r2);
+                    double l = Math.max(l1, l2);
+                    return (r+l)/2;
+                }
+            } else if (l2 > r1) {
+                low = mid1 + 1;
+            } else {
+                high = mid1 - 1;
+            }
+        }
     }
 }
